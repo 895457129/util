240222e37fdb3970ff3995cd9a6e2e1a0206964b
cd glead <cdglead@cddeiMac.local>
2020-09-18 16:48:28 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import reg from './reg';
 import fun from './fun';
 
-console.log(fun.formatPhone("18382346121"), fun.formatIDCard("500239199105105956"));
-
 export default {
   regExpUtils: reg,
   funUtils: fun,
